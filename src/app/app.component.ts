c8e9f7b76f018722fefd1043013c5dd20fc9d052
dipalihbadgujar <dipalihbadgujar16@gmail.com>
2023-11-30 07:06:40 +0530

    new changes Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(){
+    console.log("feature 2 branch is in progresss")
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'my-curd-app';
 
   message:string="welcome to Github & Git"
